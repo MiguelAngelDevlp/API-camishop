142d7f24c6d27a85961850c71a0dfdf71d047379
MiguelAngeldevlp <miguel.angel01moya@gmail.com>
2023-02-22 23:37:44 +0100

    first-commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+const teams = require("./teams.json");
+
+app.get("/", (request, response) => {
+    response.send({response: true, code: 200, teams: teams});
+});
+
+app.get("/:id", (request, response) => {
+    const { id } = request.params;
+    const results = teams.filter(team => team.id === Number(id));
+    response.status(200).send({response: true, teams: results});
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
